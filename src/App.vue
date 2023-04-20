b7fd1550dfa5e2ef0c0ae1b4c51650b9026b35df
martina <tinalai101@gmail.com>
2023-04-20 15:27:48 +0800

    feat: add learning resource list

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <ResourceList :resources="storedResources"></ResourceList>
+</template>
+
+<script>
+import ResourceList from './components/ResourceList.vue';
+
+export default {
+  components: { ResourceList },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official guide',
+          description: 'This is Vue official guide.',
+          url: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'You can search anything on Google.',
+          url: 'https://google.com'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/LearningResource.vue
@@ -0,0 +1,53 @@
+<template>
+  <li>
+    <header>
+      <h3>{{ title }}</h3>
+      <button type="button">Delete</button>
+    </header>
+    <div>
+      <p>{{ description }}</p>
+      <a :href="url">Go website</a>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['title', 'description', 'url'],
+  data() {
+
+  }
+} 
+</script>
+
+<style scoped>
+li {
+  margin: auto;
+  max-width: 40rem;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h3 {
+  font-size: 1.25rem;
+  margin: 0.5rem 0;
+}
+
+p {
+  margin: 0.5rem 0;
+}
+
+a {
+  text-decoration: none;
+  color: #ce5c00;
+}
+
+a:hover,
+a:active {
+  color: #c89300;
+}
+</style>
